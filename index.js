1ebd879c631dfddad3a55c7a3cec7529e6b19b13
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-08-04 18:22:25 -0400

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (numerator, denominator) => {
+    return numerator / denominator;
+  };
+  const result = divide(2000, 100);
+  console.log(result);
+  
+  const square = x => x * 2;{
+
+  }
+
+  const add = (a,b) =>{
+    return a+b;
+  }
+  
